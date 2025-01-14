45fbd918d449ef2ed280a912ccba1f6d44ef1bf8
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-01-14 10:24:10 +0100

    Finish hydrological analysis project

--- create_script.R
+++ create_script.R
@@ -1,7 +1,8 @@
-# Set working directory
-setwd("C:/Users/Samuel/Desktop/R-Hydrological-Analysis")
+# # Set working directory
+# setwd("C:/Users/Samuel/Desktop/R-Hydrological-Analysis")
+# 
+# getwd()
+# 
+# # Extract R code from the RMarkdown file
+# knitr::purl("Hydrological_Analysis.Rmd", output = "Hydrological_Analysis.R")
 
-getwd()
-
-# Extract R code from the RMarkdown file
-knitr::purl("Hydrological_Analysis.Rmd", output = "Hydrological_Analysis.R")
